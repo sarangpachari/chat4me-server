6652ebad51924eb77429801c1ec47ab4f6f61631
Aparna Vinod <aparnavinod083@gmail.com>
2025-03-28 19:40:39 +0530

    jwt  added in group routes

--- Router/groupRoutes.js
+++ Router/groupRoutes.js
@@ -4,6 +4,6 @@ const controller = require('../Controller/groupController')
 const jwtMiddleware = require('../Middleware/jwtMiddleware')
 
 router.post('/create-group',jwtMiddleware,controller.createGroup)
-router.get('/get-all-groups/:id',controller.getUserGroups)
+router.get('/get-all-groups/:id',jwtMiddleware,controller.getUserGroups)
 
 module.exports = router
