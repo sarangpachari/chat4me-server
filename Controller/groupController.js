e05cca818131e7e4c3412a69a68b0e88dc2a7538
kiran <kiranuthaman85@gmail.com>
2025-03-29 18:42:10 +0530

    addeda change

--- Controller/groupController.js
+++ Controller/groupController.js
@@ -70,6 +70,8 @@ exports.groupInfo = async (req, res) => {
         if (!group) {
             return res.status(400).json({ message: 'Group not found' });
         }
+        console.log("Group-info :",group);
+        
 
         res.status(200).json({
             name: group.name,
@@ -77,7 +79,7 @@ exports.groupInfo = async (req, res) => {
             groupIcon: group.groupIcon,
             groupMembers:group.groupMembers,
             groupMessages: group.groupMessages,
-            timestamps:group.timestamps
+            createdAt:group.createdAt
         });
     } catch (error) {
         console.error(error);
